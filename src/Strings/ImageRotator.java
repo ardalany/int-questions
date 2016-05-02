dfc90d38cb0843b3f3d2e1b7e7f554ed6d52b3ab
Ardalan <Ardalan@192.168.0.101>
2016-05-01 22:41:42 -0700

    added the rotation algorithm

--- src/Strings/ImageRotator.java
+++ src/Strings/ImageRotator.java
@@ -29,7 +29,23 @@ public class ImageRotator implements Runnable{
         try {
             Integer[][] matrix = _matrixReader.read();
             
+            _writer.write("\nBefore rotation:\n");
             _matrixWriter.write(matrix);
+            
+            int size = matrix.length;
+            
+            for(int offset = 0; offset < (size / 2); offset++){
+                for(int row = offset; row <= (size - 1 - offset - 1); row++){
+                    int temp = matrix[row][offset];
+                    matrix[row][offset] = matrix[size - 1 - offset][row];
+                    matrix[size - 1 - offset][row] = matrix[size - 1 - row][size - 1 - offset];
+                    matrix[size - 1 - row][size - 1 - offset] = matrix[offset][size - 1 - row];
+                    matrix[offset][size - 1 - row] = temp;
+                }
+                
+                _writer.write("\nPass " + Integer.toString(offset) + "\n");
+                _matrixWriter.write(matrix);
+            }
         } catch (Exception ex) {
             _writer.write("Exception: " + ex.getMessage());
             _writer.write(new StackTraceParser().getStackTrace(ex));
