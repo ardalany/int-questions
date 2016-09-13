125d9a36714e851cd612454fa6d5d99105c33bc6
Ardalan <Ardalan@192.168.0.101>
2016-09-12 23:38:14 -0700

    added linked list console writer

---
+++ src/Console/LinkedListWriter.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Console;
+
+import LinkedList.*;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Ardalan
+ */
+public class LinkedListWriter<T> {
+    private final OneLineWriter _writer;
+    
+    public LinkedListWriter(){
+        _writer = new OneLineWriter();
+    }
+    
+    public void write(LinkedList<T> list){
+        ArrayList<String> nodeValues = new ArrayList<>();
+        
+        LinkedListNode<T> currentNode = list.head;
+        
+        while(currentNode != null){
+            if(currentNode.data != null){
+                nodeValues.add(currentNode.data.toString());
+            }
+        }
+        
+        _writer.write(String.join(", ", nodeValues));
+    }
+}
