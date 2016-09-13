5bc3e9af448d578d97ba40677ba26c1772fa210b
Ardalan <Ardalan@192.168.0.101>
2016-09-12 23:38:49 -0700

    added linked list

---
+++ src/LinkedList/LinkedList.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package LinkedList;
+
+/**
+ *
+ * @author Ardalan
+ */
+public class LinkedList<T> {
+
+    public LinkedListNode<T> head;
+
+    public LinkedList(T headValue) {
+        head = new LinkedListNode<>(headValue);
+    }
+
+    public void appendToTail(T value) {
+        LinkedListNode<T> node = head;
+
+        while (node.next != null) {
+            node = node.next;
+        }
+
+        node.next = new LinkedListNode<>(value);
+    }
+
+    /**
+     * Deletes the first occurrence of the node with the given value.
+     *
+     * @param value the value of the node to be deleted from the list
+     */
+    public void deleteNode(T value) {
+
+        if (head.data == value) {
+            head = head.next;
+        } else {
+            LinkedListNode<T> currentNode = head;
+
+            while (currentNode.next != null && currentNode.next.data != value) {
+                currentNode = currentNode.next;
+            }
+
+            // Node was found. Delete it
+            if (currentNode.next != null) {
+                LinkedListNode<T> nodeToDelete = currentNode.next;
+
+                currentNode.next = nodeToDelete.next;
+            }
+            else{
+                throw new IndexOutOfBoundsException("a new with the provided value was not found.");
+            }
+        }
+
+    }
+}
---
+++ src/LinkedList/LinkedListNode.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package LinkedList;
+
+/**
+ *
+ * @author Ardalan
+ */
+public class LinkedListNode<T> {
+    public T data;
+    public LinkedListNode<T> next;
+    
+    public LinkedListNode(T value){
+        data = value;
+    }
+}
